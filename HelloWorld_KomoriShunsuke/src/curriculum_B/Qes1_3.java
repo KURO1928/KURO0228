c7247240b2a14e0d9da6de710829b090b78766a0
PC User <PC User@DESKTOP-HGM7JO5>
2025-06-28 23:59:00 +0900

    課題作成1_3

---
+++ HelloWorld_KomoriShunsuke/src/curriculum_B/Qes1_3.java
@@ -0,0 +1,98 @@
+package curriculum_B;
+
+import java.util.Random;
+import java.util.Scanner;
+
+
+public class Qes1_3 {
+	@SuppressWarnings("resource")
+	public static void main(String[] args) {
+
+
+		Scanner scanner = new Scanner(System.in);
+		String userName = scanner.nextLine();
+
+		/*nullまたは空文字のチェック*/
+		if (userName == null || userName.isEmpty()) {
+			System.out.println("「名前を入力してください」");
+			/*半角英数字チェック*/
+		} else if (!userName.matches("^[a-zA-Z0-9]*$")) {
+			System.out.println("「半角英数字のみで名前を入力して下さい」");
+			/*文字数チェック*/
+		} else if (userName.length() > 10) {
+			System.out.println("「10文字以内にして下さい」");
+			/*正常時*/
+		} else {
+			System.out.println("ユーザー名「" + userName + "」を登録しました");
+		}
+
+
+		/*ジャンケンゲーム*/
+		String[] hands = {"グー", "チョキ", "パー"};
+		int count = 0;
+		boolean winflag = false;
+		scanner = new Scanner(System.in);
+
+
+		/*自分の手*/
+		/*ループ始まり　ジャンケンに勝つまで続ける*/
+		while(winflag = false) {
+			count++;
+			
+			int userHand = scanner.nextInt();
+			if (userHand == 0 || userHand == 1 || userHand == 2) {
+				System.out.println( userName + "の手は「" + hands[userHand] + "」");
+			}
+
+			/*相手の手*/
+			Random random = new Random();
+			int cpuHand = random.nextInt(3);
+			System.out.println("相手の手は" + hands[cpuHand]  );
+
+			/*勝ち負けの時*/
+			/*あいこの時*/
+			if (userHand == cpuHand) {
+				System.out.println( "DRAW あいこ もう一回しましょう！");
+				continue;
+				/*勝ちの時*/
+			} else if((userHand == 0 && cpuHand == 1) || (userHand == 1 && cpuHand == 2) || (userHand == 2 && cpuHand == 0) ) {
+				System.out.println( "やるやん。");
+				System.out.println( "次は俺にリベンジさせて");
+				winflag = true;
+				break;
+				/*負けの時*/
+				/*グーに負けた時*/
+			} else if(userHand == 1 && cpuHand == 0) {
+				System.out.println( "俺の勝ち！");
+				System.out.println( "負けは次につながるチャンスです！");
+				System.out.println( "ネバーギブアップ！");
+				continue;
+				/*チョキに負けた時*/
+			} else if(userHand == 2 && cpuHand == 1) {
+				System.out.println( "俺の勝ち！");
+				System.out.println( "たかがじゃんけん、そう思ってないですか？");
+				System.out.println( "それやったら次も、俺が勝ちますよ");
+				continue;
+				/*パーに負けた時*/
+			} else if(userHand == 0 && cpuHand == 2) {
+				System.out.println( "俺の勝ち！");
+				System.out.println( "なんで負けたか、明日まで考えといてください。");
+				System.out.println( "そしたら何かが見えてくるはずです");
+				continue;
+			}
+			System.out.println("勝つまでにかかった合計回数は" + count + "回です");
+			
+		}
+
+
+		scanner.close();
+
+	}
+}	
+/*ループ文を作る　
+ * 勝ったとき（winflagがtrueの時ループを抜ける）
+ * 負けとあいこの時（winflagがfalsの場合ループを続ける）
+ * ループを抜けた後カウントを表記する
+ * */
+
+

68be82b815480f2522c695f51f2ca2e911d0b9de
PC User <PC User@DESKTOP-HGM7JO5>
2025-06-29 00:13:21 +0900

    課題作成1_3

--- HelloWorld_KomoriShunsuke/src/curriculum_B/Qes1_3.java
+++ HelloWorld_KomoriShunsuke/src/curriculum_B/Qes1_3.java
@@ -7,23 +7,30 @@ import java.util.Scanner;
 public class Qes1_3 {
 	@SuppressWarnings("resource")
 	public static void main(String[] args) {
-
+		
+		boolean trueName = false;
 
 		Scanner scanner = new Scanner(System.in);
 		String userName = scanner.nextLine();
-
+		
+		while(trueName = true)
 		/*nullまたは空文字のチェック*/
 		if (userName == null || userName.isEmpty()) {
 			System.out.println("「名前を入力してください」");
+			continue;
 			/*半角英数字チェック*/
 		} else if (!userName.matches("^[a-zA-Z0-9]*$")) {
 			System.out.println("「半角英数字のみで名前を入力して下さい」");
+			continue;
 			/*文字数チェック*/
 		} else if (userName.length() > 10) {
 			System.out.println("「10文字以内にして下さい」");
+			continue;
 			/*正常時*/
 		} else {
 			System.out.println("ユーザー名「" + userName + "」を登録しました");
+			trueName = true;
+			break;
 		}
 
 
@@ -36,7 +43,7 @@ public class Qes1_3 {
 
 		/*自分の手*/
 		/*ループ始まり　ジャンケンに勝つまで続ける*/
-		while(winflag = false) {
+		while(winflag = true) {
 			count++;
 			
 			int userHand = scanner.nextInt();
@@ -59,6 +66,7 @@ public class Qes1_3 {
 				System.out.println( "やるやん。");
 				System.out.println( "次は俺にリベンジさせて");
 				winflag = true;
+				System.out.println("勝つまでにかかった合計回数は" + count + "回です");
 				break;
 				/*負けの時*/
 				/*グーに負けた時*/
@@ -80,7 +88,6 @@ public class Qes1_3 {
 				System.out.println( "そしたら何かが見えてくるはずです");
 				continue;
 			}
-			System.out.println("勝つまでにかかった合計回数は" + count + "回です");
 			
 		}
 

9ebf1cf9d4234800ddecccb69969c7e68c0a04f8
PC User <PC User@DESKTOP-HGM7JO5>
2025-07-06 01:55:04 +0900

    課題作成5

--- HelloWorld_KomoriShunsuke/src/curriculum_B/Qes5.java
+++ HelloWorld_KomoriShunsuke/src/curriculum_B/Qes5.java
@@ -4,7 +4,7 @@ public class Qes5 {
 	 public static void main(String[] args) {
 		 for (int i = 1; i <= 9; i++) {
 	            for (int j = 1; j <= 20; j++) {
-	                // 2桁ゼロ埋めで表示
+	                // 3桁ゼロ埋めで表示
 	                System.out.print(String.format("%03d * %03d = %03d", j, i, j * i));
 	                // 9列目以外は区切りを入れる
 	                if (j != 9) {
@@ -12,6 +12,7 @@ public class Qes5 {
 	                }
 	            }
 	            System.out.println();
+	            /*もう一行あける*/
 	            System.out.println();
 	        }
 	    }

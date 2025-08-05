39dc284cfdbd5079b909cc1f845c98611386f20d
PC User <PC User@DESKTOP-HGM7JO5>
2025-08-05 23:38:46 +0900

    課題作成1_30

--- self-introduction/src/introduction/Person.java
+++ self-introduction/src/introduction/Person.java
@@ -8,23 +8,28 @@ public class Person {
 	double weight;
 	static int count = 0;
 
-	 // コンストラクタを定義しインスタンスフィールドに値をセット
-	 Person(String name , int age , double height , double weight) {
-		 this.name = name;
-		 this.age = age;
-		 this.height = height; 
-		 this.weight = weight;
-		 Person.count++;
-	 }
-	 public double bmi() {
-		 return this.weight / (this.height * this.height);
-	 }
-	 
-	 public void print() {
-	       // BMIを小数点以下1桁でフォーマット
-	       String Bmi = String.format("%.1f", this.bmi());
-	       System.out.println("名前は" + this.name + "です");
-	       System.out.println("年は" + this.age + "才です");
-	       System.out.println("BMIは" + Bmi + "です");
-	 }
+	// コンストラクタを定義しインスタンスフィールドに値をセット
+	Person(String name , int age , double height , double weight) {
+		this.name = name;
+		this.age = age;
+		this.height = height; 
+		this.weight = weight;
+		Person.count++;
+	}
+	public double bmi() {
+		return this.weight / (this.height * this.height);
+	}
+
+	public void print() {
+		double originalBmi = this.bmi();
+
+		// Math.floor()で小数点以下を切り捨て
+		double flooredBmi = Math.floor(originalBmi);
+		
+		String formatBmi = String.format("%.1f", flooredBmi);
+		
+		System.out.println("名前は" + this.name + "です");
+		System.out.println("年は" + this.age + "才です");
+		System.out.println("BMIは" + formatBmi + "です");
+	}
 }
